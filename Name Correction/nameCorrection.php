53a320523d1133c2b614b3502463928bf410916e
MarmikShah634 <mnshah632004@gmail.com>
2024-04-17 01:15:36 +0530

    Update nameCorrection.php

--- Name Correction/nameCorrection.php
+++ Name Correction/nameCorrection.php
@@ -4,6 +4,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Retrieve data from form
     $field1 = $_POST['enno'];
     $field2 = $_POST['name'];
+    $status = "waiting";
 
     // Get the current date
     $date = date('Y-m-d H:i:s');
@@ -23,11 +24,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     // Prepare SQL statement to insert data into database
-    $sql = "INSERT INTO requests(enrollment, date, name, status) VALUES (?, ?, ?, ?)";
+    $sql = "INSERT INTO `requests`(`enrollment`, `date`, `name`, `status`) VALUES (?, ?, ?, ?)";
 
     // Prepare and bind parameters
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param($field1, $date, $field2, "waiting");
+    $stmt->bind_param("ssss", $field1, $date, $field2, $status);
 
     // Execute the statement
     if ($stmt->execute() === TRUE) {
