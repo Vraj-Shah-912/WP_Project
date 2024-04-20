b36782d47afcf1e3a527e8aa28f0e524a17b8247
MarmikShah634 <mnshah632004@gmail.com>
2024-04-20 16:14:19 +0530

    caption and options of approved and disapproved added

--- Faculty Homepage/facultyHomePage.php
+++ Faculty Homepage/facultyHomePage.php
@@ -9,12 +9,14 @@
 
 <body>
     <table id="stuTable">
+        <caption style="caption-side:bottom;">Requests from student about name changing</caption>
         <thead>
             <tr>
                 <th>ID</th>
                 <th>date</th>
                 <th>name</th>
                 <th>status</th>
+                <th>options</th>
             </tr>
         </thead>
         <tbody>
@@ -34,7 +36,8 @@
 
                 if ($result->num_rows > 0) {
                     while($row = $result->fetch_assoc()) {
-                        echo "<tr><td>".$row["enrollment"]."</td><td>".$row["date"]."</td><td>".$row["name"]."</td><td>".$row["status"]."</td></tr>";
+                        echo "<tr><td>".$row["enrollment"]."</td><td>".$row["date"]."</td><td>".$row["name"]."</td><td>".$row["status"]."</td><td><button>Approve</button>
+                        <button>Disapprove</button></td></tr>";
                     }
                 } else {
                     echo "<tr><td colspan='3'>No results found</td></tr>";
