86a53bee5114610603c9617f93dec14935cfba82
MarmikShah634 <mnshah632004@gmail.com>
2024-04-20 15:50:20 +0530

    faculty home page made

---
+++ Faculty Homepage/facultyHomePage.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Faculty Homepage</title>
+</head>
+
+<body>
+    <table id="stuTable">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>date</th>
+                <th>name</th>
+                <th>status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                $servername = "localhost";
+                $username = "root";
+                $password = "";
+                $database = "wp";
+
+                $conn = new mysqli($servername, $username, $password, $database);
+
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                }
+                $sql = "SELECT * FROM requests";
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) {
+                        echo "<tr><td>".$row["enrollment"]."</td><td>".$row["date"]."</td><td>".$row["name"]."</td><td>".$row["status"]."</td></tr>";
+                    }
+                } else {
+                    echo "<tr><td colspan='3'>No results found</td></tr>";
+                }
+?>
+        </tbody>
+    </table>
+</body>
+
+</html>
